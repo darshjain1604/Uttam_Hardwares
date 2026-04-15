9b3ba963282f4402c12b2fcbb9f8b61a96e382f8
darshjain1604 <darshchajjed.80@gmail.com>
2026-04-15 14:26:30 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -204,4 +204,4 @@ def generate_qr(pid):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
